ec3af83631f41224c27aea659313a87faeeb0690
Mike Tsui <mike@aivo.ai>
2021-11-06 13:37:10 +0800

    first commit

---
+++ src/helpers/AllBonds.ts
@@ -0,0 +1,166 @@
+import { StableBond, LPBond, NetworkID, CustomBond, BondType } from "src/lib/Bond";
+import { addresses } from "src/constants";
+
+import { ReactComponent as DaiImg } from "src/assets/tokens/DAI.svg";
+import { ReactComponent as OhmDaiImg } from "src/assets/tokens/OHM-DAI.svg";
+import { ReactComponent as FraxImg } from "src/assets/tokens/FRAX.svg";
+import { ReactComponent as OhmFraxImg } from "src/assets/tokens/OHM-FRAX.svg";
+import { ReactComponent as OhmLusdImg } from "src/assets/tokens/OHM-LUSD.svg";
+import { ReactComponent as OhmEthImg } from "src/assets/tokens/OHM-WETH.svg";
+import { ReactComponent as wETHImg } from "src/assets/tokens/wETH.svg";
+import { ReactComponent as LusdImg } from "src/assets/tokens/LUSD.svg";
+
+import { abi as FraxOhmBondContract } from "src/abi/bonds/OhmFraxContract.json";
+import { abi as BondOhmDaiContract } from "src/abi/bonds/OhmDaiContract.json";
+import { abi as BondOhmLusdContract } from "src/abi/bonds/OhmLusdContract.json";
+import { abi as BondOhmEthContract } from "src/abi/bonds/OhmEthContract.json";
+
+import { abi as DaiBondContract } from "src/abi/bonds/DaiContract.json";
+import { abi as ReserveOhmLusdContract } from "src/abi/reserves/OhmLusd.json";
+import { abi as ReserveOhmDaiContract } from "src/abi/reserves/OhmDai.json";
+import { abi as ReserveOhmFraxContract } from "src/abi/reserves/OhmFrax.json";
+import { abi as ReserveOhmEthContract } from "src/abi/reserves/OhmEth.json";
+
+import { abi as FraxBondContract } from "src/abi/bonds/FraxContract.json";
+import { abi as LusdBondContract } from "src/abi/bonds/LusdContract.json";
+import { abi as EthBondContract } from "src/abi/bonds/EthContract.json";
+
+import { abi as ierc20Abi } from "src/abi/IERC20.json";
+import { getBondCalculator } from "src/helpers/BondCalculator";
+
+// TODO(zx): Further modularize by splitting up reserveAssets into vendor token definitions
+//   and include that in the definition of a bond
+export const dai = new StableBond({
+  name: "dai",
+  displayName: "DAI",
+  bondToken: "DAI",
+  isAvailable: { [NetworkID.Mainnet]: true, [NetworkID.Testnet]: true },
+  bondIconSvg: DaiImg,
+  bondContractABI: DaiBondContract,
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
+      bondAddress: "0x41d7A7E5C4f0E0Edd486e84c9F8078d59399804b",
+      reserveAddress: "0x5b3D7476F91F109Eeae5B7D31d77F01ca0caBFe8",
+    },
+    [NetworkID.Testnet]: {
+      bondAddress: "0x41d7A7E5C4f0E0Edd486e84c9F8078d59399804b",
+      reserveAddress: "0x5b3D7476F91F109Eeae5B7D31d77F01ca0caBFe8",
+    },
+  },
+});
+
+export const eth = new CustomBond({
+  name: "eth",
+  displayName: "wETH",
+  lpUrl: "",
+  bondType: BondType.StableAsset,
+  bondToken: "wETH",
+  isAvailable: { [NetworkID.Mainnet]: true, [NetworkID.Testnet]: true },
+  bondIconSvg: wETHImg,
+  bondContractABI: EthBondContract,
+  reserveContract: ierc20Abi, // The Standard ierc20Abi since they're normal tokens
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
+      bondAddress: "0xE6295201CD1ff13CeD5f063a5421c39A1D236F1c",
+      reserveAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    },
+    [NetworkID.Testnet]: {
+      bondAddress: "0xca7b90f8158A4FAA606952c023596EE6d322bcf0",
+      reserveAddress: "0xc778417e063141139fce010982780140aa0cd5ab",
+    },
+  },
+  customTreasuryBalanceFunc: async function (this: CustomBond, networkID, provider) {
+    const ethBondContract = this.getContractForBond(networkID, provider);
+    let ethPrice = await ethBondContract.assetPrice();
+    ethPrice = ethPrice / Math.pow(10, 8);
+    const token = this.getContractForReserve(networkID, provider);
+    let ethAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
+    ethAmount = ethAmount / Math.pow(10, 18);
+    return ethAmount * ethPrice;
+  },
+});
+
+export const ohm_dai = new LPBond({
+  name: "ohm_dai_lp",
+  displayName: "OHM-DAI LP",
+  bondToken: "DAI",
+  isAvailable: { [NetworkID.Mainnet]: true, [NetworkID.Testnet]: true },
+  bondIconSvg: OhmDaiImg,
+  bondContractABI: BondOhmDaiContract,
+  reserveContract: ReserveOhmDaiContract,
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
+      bondAddress: "0x956c43998316b6a2F21f89a1539f73fB5B78c151",
+      reserveAddress: "0x34d7d7Aaf50AD4944B70B320aCB24C95fa2def7c",
+    },
+    [NetworkID.Testnet]: {
+      bondAddress: "0xcF449dA417cC36009a1C6FbA78918c31594B9377",
+      reserveAddress: "0x8D5a22Fb6A1840da602E56D1a260E56770e0bCE2",
+    },
+  },
+  lpUrl:
+    "https://app.sushi.com/add/0x383518188c0c6d7730d91b2c03a03c837814a899/0x6b175474e89094c44da98b954eedeac495271d0f",
+});
+
+export const ohm_weth = new CustomBond({
+  name: "ohm_weth_lp",
+  displayName: "OHM-WETH LP",
+  bondToken: "WETH",
+  isAvailable: { [NetworkID.Mainnet]: true, [NetworkID.Testnet]: true },
+  bondIconSvg: OhmEthImg,
+  bondContractABI: BondOhmEthContract,
+  reserveContract: ReserveOhmEthContract,
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
+      bondAddress: "0xB6C9dc843dEc44Aa305217c2BbC58B44438B6E16",
+      reserveAddress: "0xfffae4a0f4ac251f4705717cd24cadccc9f33e06",
+    },
+    [NetworkID.Testnet]: {
+      // NOTE (unbanksy): using ohm-dai rinkeby contracts
+      bondAddress: "0xcF449dA417cC36009a1C6FbA78918c31594B9377",
+      reserveAddress: "0x8D5a22Fb6A1840da602E56D1a260E56770e0bCE2",
+    },
+  },
+  bondType: BondType.LP,
+  lpUrl:
+    "https://app.sushi.com/add/0x383518188c0c6d7730d91b2c03a03c837814a899/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+  customTreasuryBalanceFunc: async function (this: CustomBond, networkID, provider) {
+    if (networkID === NetworkID.Mainnet) {
+      const ethBondContract = this.getContractForBond(networkID, provider);
+      let ethPrice = await ethBondContract.assetPrice();
+      ethPrice = ethPrice / Math.pow(10, 8);
+      const token = this.getContractForReserve(networkID, provider);
+      const tokenAddress = this.getAddressForReserve(networkID);
+      const bondCalculator = getBondCalculator(networkID, provider);
+      const tokenAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
+      const valuation = await bondCalculator.valuation(tokenAddress, tokenAmount);
+      const markdown = await bondCalculator.markdown(tokenAddress);
+      let tokenUSD = (valuation / Math.pow(10, 9)) * (markdown / Math.pow(10, 18));
+      return tokenUSD * ethPrice;
+    } else {
+      // NOTE (appleseed): using OHM-DAI on rinkeby
+      const token = this.getContractForReserve(networkID, provider);
+      const tokenAddress = this.getAddressForReserve(networkID);
+      const bondCalculator = getBondCalculator(networkID, provider);
+      const tokenAmount = await token.balanceOf(addresses[networkID].TREASURY_ADDRESS);
+      const valuation = await bondCalculator.valuation(tokenAddress, tokenAmount);
+      const markdown = await bondCalculator.markdown(tokenAddress);
+      let tokenUSD = (valuation / Math.pow(10, 9)) * (markdown / Math.pow(10, 18));
+      return tokenUSD;
+    }
+  },
+});
+
+// HOW TO ADD A NEW BOND:
+// Is it a stableCoin bond? use `new StableBond`
+// Is it an LP Bond? use `new LPBond`
+// Add new bonds to this array!!
+// export const allBonds = [dai, eth, ohm_dai, ohm_weth];
+export const allBonds = [dai];
+export const allBondsMap = allBonds.reduce((prevVal, bond) => {
+  return { ...prevVal, [bond.name]: bond };
+}, {});
+
+// Debug Log
+// console.log(allBondsMap);
+export default allBonds;
